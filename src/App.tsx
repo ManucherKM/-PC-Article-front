5392afe43fdb4f2a8d1d9bd59d2e3c19f5738fbb
ManucherKM <91463158+ManucherKM@users.noreply.github.com>
2022-09-17 21:44:39 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom"
+import Admin from "./pages/Admin/Admin"
+import Error from "./pages/Error/Error"
+import Home from './pages/Home/Home'
+import Login from "./pages/Login/Login"
+import Profile from "./pages/Profile/Profile"
+import Register from "./pages/Register/Register"
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/error' element={<Error />} />
+      <Route path='/auth/me' element={<Profile />} />
+      <Route path='/admin' element={<Admin />} />
+    </Routes>
+  )
+}
+
+export default App
